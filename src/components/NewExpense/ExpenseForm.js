7dd461e3872176d7e322d174baf921404830f538
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2022-11-02 10:16:39 +0900

    to parents

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './ExpenseForm.css';
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
     const [enteredTitle, setEnteredTitle] = useState('');
     const [enteredAmount, setEnteredAmount] = useState('');
@@ -44,11 +44,11 @@ const ExpenseForm = () => {
 
         const expenseData = {
             title: enteredTitle,
-            amoutn: enteredAmount,
+            amount: enteredAmount,
             date: new Date(enteredDate)
         };
 
-        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
