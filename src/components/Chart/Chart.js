cc651ae94b24e5e3e2a9de69079e7ea9c19f70f0
송가영 <song-gayeong@song-gayeong-ui-MacBookAir.local>
2022-11-03 08:57:18 +0900

    chart complete

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,10 +3,12 @@ import './Chart.css';
 import ChartBar from './ChartBar';
 
 const Chart = (props) => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaxmum = Math.max(...dataPointValues)
   return (
     <div className='chart'>
-        {props.dataPoints.map(dataPoint => 
-        <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>)}
+        {props.dataPoints.map((dataPoint) => 
+        (<ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={totalMaxmum} label={dataPoint.label}/>))}
     </div>
   )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
@@ -41,7 +42,7 @@ const Expenses = (props) => {
       filteredExpenses.map((expense) => (
         <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
       ))} */}
-
+      <ExpensesChart expenses={filteredExpenses} />
       <ExpensesList items={filteredExpenses}/>
     </Card>
     </div>
